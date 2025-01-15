a337ea00a57b6ab4c11ef76a7823e9056111d2c5
kangourouuu <bmkhanh436@gmail.com>
2025-01-15 11:26:20 +0700

    adding UI page of character

--- fe/src/routes/index.jsx
+++ fe/src/routes/index.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import WalletConnect from "../components/WalletConnecting";
 import IntroPage from "../pages/IntroPage";
+import MainBoundary from "../components/MainBoundary";
+import Backpack from "../components/Backpack";
 
 const Router = () => {
   return (
@@ -10,6 +12,8 @@ const Router = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<WalletConnect />} />
         <Route path="/intro" element={<IntroPage />} />
+        <Route path="/main" element={<MainBoundary/>} />
+        <Route path="/backpack" element={<Backpack/>} />
       </Routes>
     </>
   );
